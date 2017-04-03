c19a2874d683217c1e318e3129bd4c25ea29a786
Chaitya Shah <Chaitya62@gmail.com>
2017-04-03 13:46:08 +0530

    Add block

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import carmodule
 def main():
     pygame.init()   #Initialize Pygame
 
-#Initializing display resolution variables
+	#Initializing display resolution variables
     display_width = 800
     display_height = 600
 
-#Initializing color tuples in format (r, g, b)
+	#Initializing color tuples in format (r, g, b)
     black = (0, 0, 0)
     white = (255, 255, 255)
 
@@ -18,15 +18,18 @@ def main():
 
     '''
         Pygame works in a loop
-        The crashed variable is a boolean flag to indicate if the loop needs\
+        The exit_game variable is a boolean flag to indicate if the loop needs\
                 to be exited.
     '''
+
     exit_game = False 
 
     car_image = pygame.image.load('racecar.png')    #Load car sprite
     car_dimensions = car_image.get_rect().size    #Get car dimensions in form of a tuple
     mycar = carmodule.car(car_image, (display_width*0.45), (display_height*0.8))    #Create object for first car with default key_bindings
-    mycar_two = carmodule.car(car_image, (display_width*0.45), (display_height*0.8), pygame.K_a, pygame.K_f)    #Create object for second car with ASDF key bindings
+    '''
+        Add two player functionality
+    '''
 
     while not exit_game:
 
@@ -34,11 +37,9 @@ def main():
             if event.type == pygame.QUIT:
                 exit_game = True
             mycar.move_car(event)
-            mycar_two.move_car(event)
 
         game_display.fill(white)
         mycar.draw_car(game_display)
-        mycar_two.draw_car(game_display)
         
         pygame.display.update() #Update the screen
                                 #If parameter is mentioned, update only that part
